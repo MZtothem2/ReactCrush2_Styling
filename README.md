a3dcb46b3e593a815df02192eab8b2d359e137ee
mztothem <47514298+MZtothem2@users.noreply.github.com>
2022-09-08 22:33:04 +0900

    S6. 'Styled Component' 활용

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ReactCrush2
 동적 스타일링
+
+- Styled Component: https://styled-components.com/
+    - js 'tagged tamplete literals' 문법을 활용
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,41 @@
-import React from 'react';
-
 import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+// [Styled components] 활용: tagged tamplete literals
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  /* 미디어쿼리도 추가 가능 */
+  @media (min-width: 768px) {
+    width: auto;
+  }
+
+
+  &:focus {
+    outline: none;
+  }
+  /* '&' 선택자 */
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
